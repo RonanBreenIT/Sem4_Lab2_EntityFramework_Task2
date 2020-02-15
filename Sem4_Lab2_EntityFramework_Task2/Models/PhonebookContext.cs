36dc4ae91a3362bd940f6afad2c3a91e34ffceb8
Ronan Breen <X00152190@mytudublin.ie>
2020-02-15 14:46:46 +0000

    Updated App.config to include Connection Strings to mdf file

--- Sem4_Lab2_EntityFramework_Task2/Models/PhonebookContext.cs
+++ Sem4_Lab2_EntityFramework_Task2/Models/PhonebookContext.cs
@@ -9,6 +9,11 @@ namespace Sem4_Lab2_EntityFramework_Task2.Models
 {
     public class PhonebookContext : DbContext
     {
+        public PhonebookContext() : base("DefaultConnection")
+        {
+            // always drop and re-create the schema
+            Database.SetInitializer<PhonebookContext>(new DropCreateDatabaseAlways<PhonebookContext>());
+        }
         public DbSet<Phonebook> Phonebooks { get; set; }
     }
 }
